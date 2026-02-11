ac53741cf1edeaa6d06e60f4944e7324a12385d0
noahbasham <noah.basham@stu.daviess.kyschools.us>
2026-02-11 20:54:01 +0000

    commit

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -9,13 +9,16 @@ public class ArrayAverages{
   }
 
   public void rowAverages(){
+    //create a variable for the average
     double average = 0;
     for(int row = 0; row<ray.length; row++)
     {
       for(int col = 0; col<ray[row].length; col++)
       {
+        //add all the numbers in each row
         average += ray[row][col];
       }
+      //calculate the average
       average = average / ray[0].length;
       System.out.println("Row " + row + ": " + average);
       average = 0;
@@ -26,13 +29,17 @@ public class ArrayAverages{
     //Column major order
     //   Take each column and go through all of the rows
     //   Top down and Left to Right
+
+    //create a variable for the average
     double average = 0;
     for(int col = 0; col<ray[0].length; col++)
     {
       for(int row = 0; row<ray.length; row++)
       {
+        //add all the numbers in each column
         average += ray[row][col];
       }
+      //calculate the average
       average = average / ray.length;
       System.out.println("Column " + col + ": " + average);
       average = 0;
@@ -40,14 +47,17 @@ public class ArrayAverages{
   }
 
   public void arrayAverage(){
+    //create a variable for the average
     double average = 0;
     for(int row = 0; row<ray.length; row++)
     {
       for(int col = 0; col<ray[row].length; col++)
       {
+        //add all the numbers the array
         average += ray[row][col];
       }
     }
+    //calculate the average
     average = average / (ray.length * ray[0].length);
     System.out.println("The array's average is: " + average);
   }
