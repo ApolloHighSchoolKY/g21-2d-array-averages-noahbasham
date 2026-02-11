b1b9adcc5f353301a6f9be241ee10decd5b33967
noahbasham <noah.basham@stu.daviess.kyschools.us>
2026-02-11 20:27:10 +0000

    commit

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -9,17 +9,47 @@ public class ArrayAverages{
   }
 
   public void rowAverages(){
-    
+    double average = 0;
+    for(int row = 0; row<ray.length; row++)
+    {
+      for(int col = 0; col<ray[row].length; col++)
+      {
+        average += ray[row][col];
+      }
+      average = average / ray[0].length;
+      System.out.println("Row " + row + ": " + average);
+      average = 0;
+    }
   }
 
   public void columnAverages(){
     //Column major order
     //   Take each column and go through all of the rows
     //   Top down and Left to Right
+    double average = 0;
+    for(int col = 0; col<ray[0].length; col++)
+    {
+      for(int row = 0; row<ray.length; row++)
+      {
+        average += ray[row][col];
+      }
+      average = average / ray.length;
+      System.out.println("Column " + col + ": " + average);
+      average = 0;
+    }
   }
 
   public void arrayAverage(){
-
+    double average = 0;
+    for(int row = 0; row<ray.length; row++)
+    {
+      for(int col = 0; col<ray[row].length; col++)
+      {
+        average += ray[row][col];
+      }
+    }
+    average = average / (ray.length * ray[0].length);
+    System.out.println("The array's average is: " + average);
   }
 
   public String toString(){
--- Gee21.java
+++ Gee21.java
@@ -10,7 +10,15 @@ public class Gee21{
       15 16 17 18 19 20 21
       22 23 24 25...
       */
-
+    int x = 1;
+    for(int row = 0; row<ray.length; row++)
+    {
+      for(int col = 0; col<ray[row].length; col++)
+        {
+          ray[row][col] = x;
+          x++;
+        }
+    }
     ArrayAverages twoDee = new ArrayAverages(ray);
 
     System.out.println(twoDee);
